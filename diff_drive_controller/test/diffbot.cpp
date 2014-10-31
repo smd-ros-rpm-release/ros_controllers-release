507c4ced1b4b9f76b097ae8e5feb62cc82d1eb74
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2014-10-31 16:47:14 +0100

    Rebase from 'upstream'

--- diff_drive_controller/test/diffbot.cpp
+++ diff_drive_controller/test/diffbot.cpp
@@ -29,77 +29,18 @@
 
 // ROS
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
 
 // ros_control
 #include <controller_manager/controller_manager.h>
-#include <hardware_interface/joint_command_interface.h>
-#include <hardware_interface/joint_state_interface.h>
-#include <hardware_interface/robot_hw.h>
-#include <realtime_tools/realtime_buffer.h>
 
-class Diffbot : public hardware_interface::RobotHW
-{
-public:
-  Diffbot()
-  {
-    // Intialize raw data
-    pos_[0] = 0.0; pos_[1] = 0.0;
-    vel_[0] = 0.0; vel_[1] = 0.0;
-    eff_[0] = 0.0; eff_[1] = 0.0;
-    cmd_[0] = 0.0; cmd_[1] = 0.0;
-
-    // Connect and register the joint state interface
-    hardware_interface::JointStateHandle state_handle_1("joint_w1", &pos_[0], &vel_[0], &eff_[0]);
-    jnt_state_interface_.registerHandle(state_handle_1);
-
-    hardware_interface::JointStateHandle state_handle_2("joint_w2", &pos_[1], &vel_[1], &eff_[1]);
-    jnt_state_interface_.registerHandle(state_handle_2);
-
-    registerInterface(&jnt_state_interface_);
-
-    // Connect and register the joint velocity interface
-    hardware_interface::JointHandle vel_handle_1(jnt_state_interface_.getHandle("joint_w1"), &cmd_[0]);
-    jnt_vel_interface_.registerHandle(vel_handle_1);
-
-    hardware_interface::JointHandle vel_handle_2(jnt_state_interface_.getHandle("joint_w2"), &cmd_[1]);
-    jnt_vel_interface_.registerHandle(vel_handle_2);
-
-    registerInterface(&jnt_vel_interface_);
-  }
-
-  ros::Time getTime() const {return ros::Time::now();}
-  ros::Duration getPeriod() const {return ros::Duration(0.01);}
-
-  void read()
-  {
-    ROS_INFO_STREAM("Commands for joints: " << cmd_[0] << ", " << cmd_[1]);
-  }
-
-  void write()
-  {
-    for (unsigned int i = 0; i < 2; ++i)
-    {
-      pos_[i] += vel_[i]*getPeriod().toSec(); // update position
-      vel_[i] = cmd_[i]; // might add smoothing here later
-    }
-  }
-
-private:
-  hardware_interface::JointStateInterface    jnt_state_interface_;
-  hardware_interface::VelocityJointInterface jnt_vel_interface_;
-  double cmd_[2];
-  double pos_[2];
-  double vel_[2];
-  double eff_[2];
-};
+#include "diffbot.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "diffbot");
   ros::NodeHandle nh;
 
-  Diffbot robot;
+  Diffbot<> robot;
   ROS_WARN_STREAM("period: " << robot.getPeriod().toSec());
   controller_manager::ControllerManager cm(&robot, nh);
 
